cbca6d387607e7d2bbd1d98bffbfbcc0855687b7
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-03 18:48:39 -0700

    config(installer): define target PHP runtimes

--- config/installer.php
+++ config/installer.php
@@ -8,4 +8,29 @@ return [
     'minimum_token_ttl_minutes' => 5,
     'maximum_token_ttl_minutes' => 60,
     'unlock_session_ttl_minutes' => 15,
+
+    'php_runtimes' => [
+        'cli_binary' => env('INSTALLER_PHP_CLI_BINARY', '/www/server/php/84/bin/php'),
+        'lsphp_binary' => env('INSTALLER_PHP_LSPHP_BINARY', '/usr/local/lsws/lsphp84/bin/lsphp'),
+        'required_extensions' => [
+            'common' => [
+                'bcmath',
+                'curl',
+                'dom',
+                'fileinfo',
+                'intl',
+                'json',
+                'mbstring',
+                'openssl',
+                'pdo',
+                'pdo_mysql',
+                'redis',
+                'sodium',
+                'tokenizer',
+                'xml',
+            ],
+            'cli' => ['pcntl'],
+            'lsphp' => [],
+        ],
+    ],
 ];
