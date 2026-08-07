6322d6cf59f632d863883f4c16c1a749ad2665c5
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-07 05:08:43 -0700

    feat(panels): add authoritative lookup unavailable exception

---
+++ app/Modules/Panels/Application/Exceptions/AuthoritativePanelLookupUnavailable.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Panels\Application\Exceptions;
+
+use RuntimeException;
+
+final class AuthoritativePanelLookupUnavailable extends RuntimeException {}
