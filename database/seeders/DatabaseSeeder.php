6814227705370b2b1adc70ffec56ec1f87c718bd
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-07 22:36:22 -0700

    feat: register wallet access seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ final class DatabaseSeeder extends Seeder
             IdentityAccessFoundationSeeder::class,
             CatalogAccessFoundationSeeder::class,
             PanelsAccessFoundationSeeder::class,
+            WalletAccessFoundationSeeder::class,
         ]);
     }
 }
