4a464e6e8fcaf9c5c696dd2a425512f9ae363fff
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-07 23:19:46 -0700

    refactor: centralize wallet system account codes

---
+++ app/Modules/Wallet/Domain/WalletSystemAccountCode.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Wallet\Domain;
+
+final class WalletSystemAccountCode
+{
+    public const CORRECTION_OFFSET = 'system.wallet.correction.offset';
+
+    private function __construct() {}
+}
