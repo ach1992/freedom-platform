394f817420712c37b4181c15d8e226f91c07ee41
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-10 15:02:05 -0700

    PAY-001 v2: name rule administrator foreign key

--- database/migrations/2026_08_10_003800_create_payment_method_eligibility_foundation.php
+++ database/migrations/2026_08_10_003800_create_payment_method_eligibility_foundation.php
@@ -61,7 +61,8 @@ return new class extends Migration
             $table->char('request_payload_hash', 64);
             $table->json('configuration_snapshot');
             $table->char('configuration_snapshot_hash', 64);
-            $table->foreignId('changed_by_administrator_id')->constrained('administrators')->restrictOnDelete();
+            $table->unsignedBigInteger('changed_by_administrator_id');
+            $table->foreign('changed_by_administrator_id', 'payment_rule_admin_fk')->references('id')->on('administrators')->restrictOnDelete();
             $table->string('change_reason', 255);
             $table->char('correlation_id', 64);
             $table->dateTime('created_at', 6);
