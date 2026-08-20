c8cfab69388b7ce373bf357250d616f694359ce5
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-20 20:02:07 +0800

    Restore literal SQL migration contract

--- database/migrations/2026_08_20_000110_enable_paid_service_mutation_authority.php
+++ database/migrations/2026_08_20_000110_enable_paid_service_mutation_authority.php
@@ -79,7 +79,7 @@ SQL);
         $this->dropPaidUpgradeFence();
     }
 
-    /** @return non-empty-string */
+    /** @return literal-string */
     private function sqlFile(string $relativePath, string $unavailableMessage): string
     {
         $sql = file_get_contents(database_path($relativePath));
@@ -87,6 +87,7 @@ SQL);
             throw new RuntimeException($unavailableMessage);
         }
 
+        /** @var literal-string $sql */
         return $sql;
     }
 
