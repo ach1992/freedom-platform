df8eddeba059e34ccead6c144988c169e7857ea3
ACh <58507994+ach1992@users.noreply.github.com>
2026-08-22 15:10:11 -0700

    Clean audit authority trigger on interrupted install

--- database/migrations/2026_08_21_000200_enable_service_auto_renew_authority.php
+++ database/migrations/2026_08_21_000200_enable_service_auto_renew_authority.php
@@ -240,6 +240,7 @@ return new class extends Migration
         DB::unprepared('DROP TRIGGER IF EXISTS sarph_delete_guard');
         DB::unprepared('DROP TRIGGER IF EXISTS sarch_update_guard');
         DB::unprepared('DROP TRIGGER IF EXISTS sarch_delete_guard');
+        DB::unprepared('DROP TRIGGER IF EXISTS sarae_insert_authority_guard');
         DB::unprepared('DROP TRIGGER IF EXISTS sarae_update_guard');
         DB::unprepared('DROP TRIGGER IF EXISTS sarae_delete_guard');
         DB::unprepared('DROP TRIGGER IF EXISTS sarni_insert_authority_guard');
