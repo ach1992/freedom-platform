0f61b3cee8e425d352e8c60b860b75073f93ea78
ACh <58507994+ach1992@users.noreply.github.com>
2026-09-05 01:11:04 +0330

    Keep existing Telegram C2C payment contract stable

--- app/Modules/Telegram/Application/Contracts/TelegramCustomerPurchaseCardToCardPayment.php
+++ app/Modules/Telegram/Application/Contracts/TelegramCustomerPurchaseCardToCardPayment.php
@@ -6,8 +6,6 @@ namespace App\Modules\Telegram\Application\Contracts;
 
 use App\Modules\Telegram\Application\TelegramCustomerPurchaseCardToCardDestination;
 use App\Modules\Telegram\Application\TelegramCustomerPurchaseCardToCardReservation;
-use App\Modules\Telegram\Application\TelegramCustomerPurchaseCardToCardSubmission;
-use DateTimeImmutable;
 
 interface TelegramCustomerPurchaseCardToCardPayment
 {
@@ -27,14 +25,4 @@ interface TelegramCustomerPurchaseCardToCardPayment
         int $subjectUserId,
         string $reservationPublicId,
     ): TelegramCustomerPurchaseCardToCardDestination;
-
-    public function submitReceiptForSelf(
-        int $actorUserId,
-        int $subjectUserId,
-        string $reservationPublicId,
-        DateTimeImmutable $submittedAt,
-        string $evidenceHash,
-        string $privateReceiptReference,
-        string $operationKey,
-    ): TelegramCustomerPurchaseCardToCardSubmission;
 }
