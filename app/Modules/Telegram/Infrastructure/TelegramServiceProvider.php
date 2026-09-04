a3ef6569690d468d1230df035bad67c633d98063
ACh <58507994+ach1992@users.noreply.github.com>
2026-09-05 01:09:09 +0330

    Bind Telegram private media fetcher

--- app/Modules/Telegram/Infrastructure/TelegramServiceProvider.php
+++ app/Modules/Telegram/Infrastructure/TelegramServiceProvider.php
@@ -10,6 +10,7 @@ use App\Modules\Telegram\Application\Contracts\TelegramBotApi;
 use App\Modules\Telegram\Application\Contracts\TelegramDeliveryRuntime;
 use App\Modules\Telegram\Application\Contracts\TelegramInteractionHandler;
 use App\Modules\Telegram\Application\Contracts\TelegramMutationTransport;
+use App\Modules\Telegram\Application\Contracts\TelegramPrivateMediaFetcher;
 use App\Modules\Telegram\Application\Contracts\TelegramRuntime;
 use App\Modules\Telegram\Application\TelegramConfidentialDeliveryOutboxHandler;
 use App\Modules\Telegram\Application\TelegramConfidentialPresentationHasher;
@@ -93,6 +94,13 @@ final class TelegramServiceProvider extends ServiceProvider
         $this->app->singleton(
             TelegramBotApi::class,
             fn (Application $application): TelegramBotApi => new HttpTelegramBotApi(
+                $application->make(Factory::class),
+                $application->make(TelegramRuntimeConfiguration::class),
+            ),
+        );
+        $this->app->singleton(
+            TelegramPrivateMediaFetcher::class,
+            fn (Application $application): TelegramPrivateMediaFetcher => new HttpTelegramPrivateMediaFetcher(
                 $application->make(Factory::class),
                 $application->make(TelegramRuntimeConfiguration::class),
             ),
