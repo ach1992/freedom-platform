74a047742832ead0e00460a0eb0e4d69b57dd48d
ACh <58507994+ach1992@users.noreply.github.com>
2026-09-05 01:09:23 +0330

    Authorize reviewed C2C receipt status gateway

--- app/Modules/Telegram/Application/TelegramConfidentialPresentationProvenanceGuard.php
+++ app/Modules/Telegram/Application/TelegramConfidentialPresentationProvenanceGuard.php
@@ -19,6 +19,7 @@ final class TelegramConfidentialPresentationProvenanceGuard
     /** @var list<string> */
     public const REVIEWED_SOURCE_FILES = [
         'app/Modules/Telegram/Application/TelegramNavigationHandler.php',
+        'app/Modules/Telegram/Application/TelegramPrivateMediaInteractionGateway.php',
     ];
 
     /** @var list<string> */
