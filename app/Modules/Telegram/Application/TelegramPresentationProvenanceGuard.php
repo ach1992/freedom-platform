7432e5e243d10a56aff744eab9b9851e8293297c
ACh <58507994+ach1992@users.noreply.github.com>
2026-09-10 01:26:40 +0330

    Fix Zarinpal presentation provenance registration

--- app/Modules/Telegram/Application/TelegramPresentationProvenanceGuard.php
+++ app/Modules/Telegram/Application/TelegramPresentationProvenanceGuard.php
@@ -26,6 +26,7 @@ final class TelegramPresentationProvenanceGuard
         'app/Modules/Telegram/Application/TelegramProtectedReferenceDeliveryOutboxHandler.php',
         'app/Modules/Telegram/Application/TelegramNavigationHandler.php',
         'app/Modules/Telegram/Application/TelegramUsdtNavigationHandler.php',
+        'app/Modules/Telegram/Application/TelegramZarinpalNavigationHandler.php',
     ];
 
     /** @var list<string> */
