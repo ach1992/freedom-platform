1cd9bdb5e455d98e4fee6e2c01b1cdcf9bb63b3a
ACh <58507994+ach1992@users.noreply.github.com>
2026-09-11 14:22:27 +0800

    test(telegram): fix redis capture setup

--- tests/Feature/TelegramChannelMembershipEvaluationTest.php
+++ tests/Feature/TelegramChannelMembershipEvaluationTest.php
@@ -450,9 +450,8 @@ final class TelegramChannelMembershipEvaluationTest extends TestCase
 
         /** @var list<string> $redisCommands */
         $redisCommands = [];
-        foreach (array_keys(Redis::connections()) as $connectionName) {
-            Redis::purge($connectionName);
-        }
+        Redis::purge('default');
+        Redis::purge('cache');
         Redis::enableEvents();
         $redis = Redis::connection('cache');
         $redis->listen(static function (CommandExecuted $command) use (&$redisCommands): void {
